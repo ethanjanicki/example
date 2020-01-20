e7cd4f175f0e2e8a41c9ad7443b433cf730c1b75
ethanjanicki <ethanjanicki89@knights.ucf.edu>
2020-01-19 21:08:13 -0500

    Files for first assign

---
+++ janicki 01/src/janicki_01/GoodbyeWolrd.java
@@ -0,0 +1,12 @@
+package janicki_01;
+
+public class GoodbyeWolrd {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			
+			
+			
+			System.out.printf("Goodbye World!");
+			
+	}
+}
